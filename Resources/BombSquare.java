c57cdcb39eea7185add7b954dc0a0000f655ea21
BenGrayU <b.gray4@lancaster.ac.uk>
2023-11-02 14:24:04 +0000

    Completed task 1 got in to a rhythm so forgot to commit for a while. need to add game over :)

--- Resources/BombSquare.java
+++ Resources/BombSquare.java
@@ -1,9 +1,12 @@
+import javax.swing.*;
 import java.util.*;
 
 public class BombSquare extends GameSquare
 {
 	private boolean thisSquareHasBomb = false;
 	public static final int MINE_PROBABILITY = 10;
+
+	private boolean clicked = false;
 
 	public BombSquare(int x, int y, GameBoard board)
 	{
@@ -15,5 +18,84 @@ public class BombSquare extends GameSquare
 
 	public void clicked()
 	{
+		if(thisSquareHasBomb){
+			setImage("Resources/images/bomb.png");
+			gameOver();
+		}else{
+			checkAround();
+		}
 	}
+
+	public void checkAround(){
+		this.clicked = true;
+		int bombs = 0;
+		for (int i = this.xLocation-1; i <= this.xLocation+1; i++) {
+			for (int j = this.yLocation-1; j <= this.yLocation+1; j++) {
+				BombSquare s = (BombSquare) board.getSquareAt(i, j);
+				if(s != null && s.isBomb()){
+					bombs++;
+				}
+			}
+		}
+		if(bombs == 0){
+			for (int i = this.xLocation-1; i <= this.xLocation+1; i++) {
+				for (int j = this.yLocation-1; j <= this.yLocation+1; j++) {
+					BombSquare s = (BombSquare) board.getSquareAt(i, j);
+					if(s != null && s.clicked == false){ s.checkAround(); }
+				}
+			}
+		}
+		setImage(updateImage(bombs));
+
+
+	}
+
+	public String updateImage(int bombs){
+		String image = "Resources/images/";
+		switch (bombs){
+			case 1:
+				image = image.concat("1.png");
+				break;
+			case 2:
+				image = image.concat("2.png");
+				break;
+			case 3:
+				image = image.concat("3.png");
+				break;
+			case 4:
+				image = image.concat("4.png");
+				break;
+			case 5:
+				image = image.concat("5.png");
+				break;
+			case 6:
+				image = image.concat("6.png");
+				break;
+			case 7:
+				image = image.concat("7.png");
+				break;
+			case 8:
+				image = image.concat("8.png");
+				break;
+			default:
+				image = image.concat("0.png");
+				break;
+		}
+		return image;
+	}
+
+	public void gameOver(){
+
+	}
+
+	public boolean isBomb(){
+		return thisSquareHasBomb;
+	}
+
+	public boolean isClicked(){
+		return clicked;
+	}
+
+
+
 }
